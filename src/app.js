ec54d08094a8f00bf1be4029e67f03b92ed13b55
suyash chouksey <suyash1818chouksey@gmail.com>
2023-11-05 21:35:43 +0530

    get

--- src/app.js
+++ src/app.js
@@ -35,27 +35,7 @@ app.get('/', (req, res) => {
  *               - _id: 1
  *                 name: John Doe
  *                 subscribedChannel: Channel A
- *   post:
- *     summary: Create a new subscriber.
- *     description: Create a new subscriber.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *               subscribedChannel:
- *                 type: string
- *     responses:
- *       201:
- *         description: Subscriber created successfully
- *       400:
- *         description: Bad Request
- *       500:
- *         description: Internal Server Error
+ *   
  */
 app.get('/subscribers', async (req, res) => {
   try {
@@ -66,22 +46,7 @@ app.get('/subscribers', async (req, res) => {
   }
 });
 
-app.post('/subscribers', async (req, res) => {
-  try {
-    const { name, subscribedChannel } = req.body;
 
-    if (!name || !subscribedChannel) {
-      return res.status(400).json({ message: 'Name and subscribedChannel are required.' });
-    }
-
-    const newSubscriber = new Subscriber({ name, subscribedChannel });
-    await newSubscriber.save();
-
-    res.status(201).json(newSubscriber);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 
 
 /**
@@ -95,27 +60,7 @@ app.post('/subscribers', async (req, res) => {
  *         description: Successful response
  *       500:
  *         description: Internal Server Error
- *   post:
- *     summary: Create a new subscriber's name and subscribed channel.
- *     description: Create a new subscriber's name and their subscribed channel.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *               subscribedChannel:
- *                 type: string
- *     responses:
- *       201:
- *         description: Subscriber created successfully
- *       400:
- *         description: Bad Request
- *       500:
- *         description: Internal Server Error
+ *  
  */
 app.get('/subscribers/name', async (req, res) => {
   try {
@@ -126,22 +71,7 @@ app.get('/subscribers/name', async (req, res) => {
   }
 });
 
-app.post('/subscribers/name', async (req, res) => {
-  try {
-    const { name, subscribedChannel } = req.body;
-    
-    if (!name || !subscribedChannel) {
-      return res.status(400).json({ message: 'Name and subscribedChannel are required.' });
-    }
 
-    const newSubscriber = new Subscriber({ name, subscribedChannel });
-    await newSubscriber.save();
-
-    res.status(201).json(newSubscriber);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
 
 
 /**
@@ -162,34 +92,7 @@ app.post('/subscribers/name', async (req, res) => {
  *         description: Successful response
  *       400:
  *         description: Subscriber not found
- *   post:
- *     summary: Create a subscriber with a specific ID.
- *     description: Create a new subscriber with the provided ID.
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: The ID for the new subscriber.
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *               subscribedChannel:
- *                 type: string
- *     responses:
- *       201:
- *         description: Subscriber created successfully
- *       400:
- *         description: Bad Request
- *       500:
- *         description: Internal Server Error
+ *   
  */
 app.get('/subscribers/:id', async (req, res) => {
   try {
@@ -203,24 +106,8 @@ app.get('/subscribers/:id', async (req, res) => {
   }
 });
 
-app.post('/subscribers/:id', async (req, res) => {
-  try {
-    const { name, subscribedChannel } = req.body;
-    const id = req.params.id;
 
-    if (!name || !subscribedChannel) {
-      return res.status(400).json({ message: 'Both name and subscribedChannel are required.' });
-    }
-
-    // Create a new subscriber with the provided ID
-    const newSubscriber = new Subscriber({ _id: id, name, subscribedChannel });
-    await newSubscriber.save();
-
-    res.status(201).json(newSubscriber);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+;
 
 
 // Swagger configuration
@@ -233,7 +120,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'https://get-youtube-subscribers-p7ok.onrender.com' 
+      url: 'https://youtube-subscribers-a9z5.onrender.com' 
     },
   ],
 };
